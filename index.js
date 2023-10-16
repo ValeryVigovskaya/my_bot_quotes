7277bbdcc5e4508d35e726574f304d02872090c4
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2023-10-17 00:17:56 +0300

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const TelegramApi = require('node-telegram-bot-api');
+const {array} = require('./quotes')
+const {token} = require('./config')
+
+
+const bot = new TelegramApi(token, { polling: true });
+
+const randQuote = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const start = () => {
+    bot.setMyCommands([
+        { command: '/start', description: 'Начальное приветствие' },
+        { command: '/info', description: 'Получить информацию о пользователе' },
+        { command: '/i_want_a_quote', description: 'Получить рандомную цитату' }
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        if (text === '/start') {
+            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/06c/d14/06cd1435-9376-40d1-b196-097f5c30515c/1.webp')
+            return bot.sendMessage(chatId, `Добро пожаловать в телеграм-бот Леры`)
+        }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name}`)
+        }
+        if (text === '/i_want_a_quote') {
+            return bot.sendMessage(chatId, `${randQuote(array)}`)
+        }
+        return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй еще раз!`)
+    })
+}
+
+start()
